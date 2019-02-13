95bcbe8ab4f9fa1327aca81d046e5422fe4be240
Sindy <sindy@vidku.com>
2019-02-13 03:09:51 -0600

    update portfolio
    updated portfolio

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -1,19 +1,19 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
     
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navBar");
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById("navBar");
+// var sticky = navbar.offsetTop;
 
-function myFunction() {
+// function myFunction() {
 
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
 
 
 
-});
+// });
